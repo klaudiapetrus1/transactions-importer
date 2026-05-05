232f91c8cc189976a70b68e4c2b65b99222133b7
klaudiapetrus1 <klaudiapetrus1@gmail.com>
2026-05-05 15:24:13 +0200

    init project

---
+++ src/main/java/com/klaudia/transactionsimporter/TransactionsImporterApplication.java
@@ -0,0 +1,13 @@
+package com.klaudia.transactionsimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsImporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klaudia/transactionsimporter/TransactionsImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaudia.transactionsimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
